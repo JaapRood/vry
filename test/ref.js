a098ec6d237b273e1ec4816012e28942f27cea65
Jaap Rood <jaap@jaaprood.nl>
2016-04-22 21:12:58 +1000

    Testing `Ref.resolve`

--- test/ref.js
+++ test/ref.js
@@ -1,6 +1,6 @@
 const Test = require('tape');
 const Immutable = require('immutable');
-const _ = require('lodash');
+const _isUndefined = require('lodash.isundefined');
 
 const State = require('../lib/state');
 const KeyPath = require('../lib/key-path');
@@ -18,4 +18,22 @@ Test('Ref.create', function(t) {
 		t.deepEqual(ref.get('path').toArray(), path, 'path of returned instance is equivalent to the one passed in');
 	}, 'accepts any value that can be parsed as a KeyPath');
 
+});
+
+Test('Ref.resolve', function(t) {
+	t.plan(3);
+
+	const source = Immutable.Map({
+		a: Immutable.Map({
+			b: 'c'
+		})
+	})
+
+	const ref = Ref.create(['a', 'b']);
+	const invalidRef = Ref.create(['a', 'd', 'e']);
+
+	t.doesNotThrow(function() {
+		t.equal(Ref.resolve(ref, source), 'c', 'returns the value of the source, at the path specified by the ref');
+		t.ok(_isUndefined(Ref.resolve(invalidRef, source)), 'returns undefined for refs with paths that do not exist in the source state');
+	}, 'accepts a Ref instance and an immutable iterable');
 });
