eda0272b974d7f723f9511285092415a9fc98e56
Jaap Rood <jaap@jaaprood.nl>
2016-06-12 17:42:11 +1000

    Implemented a first crude version of the Model, accepting a spec object with defaults and a Schema of nested entities, to implement a `parse` method which the `factory` will use

--- src/model.js
+++ src/model.js
@@ -1,24 +1,134 @@
+const Immutable = require('immutable')
+const Invariant = require('invariant')
+const _assign = require('lodash.assign')
+const _every = require('lodash.every')
+const _isArray = require('lodash.isarray')
+const _isPlainObject = require('lodash.isplainobject')
+const _isFunction = require('lodash.isfunction')
+
+const Identity = require('./identity')
+const Factory = require('./factory')
+
 const internals = {}
 
 exports.isModel = (maybeModel) => {}
 
-exports.create = (name, schema) => {
-	// verify schema
+exports.create = (spec) => {
+	Invariant(
+		_isPlainObject(spec) &&
+		spec.name && (typeof spec.name === "string")
+	, 'A plain object (spec) with a name is required to create a model')
 
-	const defaults = Schema.getDefaults(schema)
+	const {
+		name,
+		defaults
+	} = spec
 
-	const identity = Identity.create(name)
-	const factory = Factory.create(defaults)
+	let schema = spec.schema
+
+	Invariant(!defaults || Factory.isDefaults(defaults), 'When specifying defaults in the model spec it must be a plain object or Immutable Iterable')
+	Invariant(!schema || Schema.isSchema(schema), 'When specificying a schema for a model, it must be a valid schema definition')
+	
+	if (!schema) schema = {}
 
 	const modelPrototype = _assign(
 		Identity.create(name),
 		Factory.create(defaults),
-		internals.createParser(schema),
-		internals.createMerger(schema),
-		intenrals.createSerializer(serializer)
+		{
+			schema: () => schema
+		},
+		{
+			parse: exports.parse,
+			merge: exports.merge,
+			serialize: exports.serialize
+		}
 	)
 
 	let model = Object.create(modelPrototype)
 	
 	return model
 }
+
+exports.parse = function(attrs, options={}) {
+	const schema = options.schema || this.schema()
+
+	return attrs.map((modelValue, modelProp) => {
+		const definition = Schema.getDefinition(schema, modelProp)
+
+		// if no type was defined for this prop there is nothing for us to do
+		if (!modelValue || !definition) return modelValue;
+
+		if (Schema.isType(definition)) {
+			let type = definition
+
+			if (Schema.instanceOfType(type, modelValue)) {
+				// if the value is already and instance of what we're trying to make it
+				// there is nothing for us to do
+				return modelValue
+			} else {
+				return type.factory(modelValue)
+			}
+		} else if (Schema.isSchema(definition)) {
+			let nestedSchema = definition
+
+			if (
+				Immutable.Iterable.isIndexed(modelValue) && _isArray(nestedSchema) ||
+				Immutable.Iterable.isKeyed(modelValue) && _isPlainObject(nestedSchema)
+			) {
+				return exports.parse(modelValue, { schema: nestedSchema })
+			} else {
+				if (process.env.name !== 'production') {
+					Warning(`Value for '${stateProp}' ignored because it did not match the type defined in schema`);
+				}
+				return null;
+			}
+		} else {
+			return modelValue
+		}
+	})
+}
+
+
+const Schema = {
+	getDefinition(schema, prop) {
+		return _isArray(schema) ? schema[0] : 
+				schema ? schema[prop] :
+				null
+	},
+
+	isSchema(maybeSchema) {
+		return !!(
+			// objects go
+			_isPlainObject(maybeSchema) || 
+			// array with one value go
+			_isArray(maybeSchema) && maybeSchema.length === 1
+		) && _every(maybeSchema, (value, key) => {
+			return Schema.isType(value) || Schema.isSchema(value)
+		})
+	},
+
+	isType(maybeType) {
+		return !!(maybeType && _isFunction(maybeType.factory))
+	},
+
+	instanceOfType(type, maybeInstance) {
+		// const type = definition.type
+		return (
+			type && 
+			_isFunction(type.instanceOf) && 
+			type.instanceOf(maybeInstance)
+		)
+	},
+
+	getType(definition) {
+
+	}
+}
+
+// exports.isSchema = (maybeSchema) => {}
+// exports.isEntity = (maybeEntity) => {
+// 	return maybeEntity && _isFunction(maybeEntity.factory)
+// }
+
+exports.merge = () => {}
+exports.serialize = () => {}
